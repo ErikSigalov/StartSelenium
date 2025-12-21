16f71904d914eadb645e403b28fc7bedce1f7356
ErikSigalov <sigaloverik@gmail.com>
2025-12-21 16:29:50 +0200

    xpath

--- StartSelenium/src/test/java/TableTest.java
+++ StartSelenium/src/test/java/TableTest.java
@@ -22,6 +22,7 @@ public class TableTest {
     @Test
     public void tableTestCss(){
         WebElement canada = wd.findElement(By.cssSelector("tr:nth-child(8) td:last-child"));
+        wd.findElement(By.xpath("//tr[8]/td[last()]"));
         String text = canada.getText();
 
         System.out.println(text);
@@ -32,6 +33,7 @@ public class TableTest {
 
     public void tableHomeWork(){
         WebElement row4 = wd.findElement(By.cssSelector("tr:nth-child(4)"));
+        wd.findElement(By.xpath("//tr[4]"));
         String text1 = row4.getText();
 
         System.out.println(text1);
@@ -42,6 +44,7 @@ public class TableTest {
 
     public void tableHomeWork1(){
         WebElement francisco = wd.findElement(By.cssSelector("tr:nth-child(4) td:nth-child(2)"));
+        wd.findElement(By.xpath("//tr[4]//td[2]"));
         String text2 = francisco.getText();
 
         System.out.println(text2);
@@ -54,16 +57,19 @@ public class TableTest {
     public void tableHomeWork2(){
         //print count of rows
         List<WebElement> rows = wd.findElements(By.cssSelector("tr"));
+        wd.findElement(By.xpath("//tr"));
         System.out.println(rows.size());
 
         Assert.assertEquals(rows.size(), 28);
 
         //print count of columns
         List<WebElement> columns = wd.findElements(By.cssSelector("tr th"));
+        wd.findElement(By.xpath("//tr/th"));
         System.out.println(columns.size());
 
         //print row 3
         List<WebElement> row3 = wd.findElements(By.cssSelector("tr:nth-child(3) td"));
+        wd.findElement(By.xpath("//tr[3]/td"));
         System.out.println("Row 3 --->" + row3.size());
 
         for(WebElement el: row3) {
@@ -72,6 +78,7 @@ public class TableTest {
 
         //print last columns
         List<WebElement> lastColumns = wd.findElements(By.cssSelector("td:last-child"));
+        wd.findElements(By.xpath("//td[last()]"));
         for (WebElement el:
         lastColumns){
             System.out.println(el.getText());
@@ -79,6 +86,7 @@ public class TableTest {
 
         //print MAaria Anders
         System.out.println(wd.findElement(By.cssSelector("tr:nth-child(2) td:nth-child(2)")).getText());
+        wd.findElement(By.xpath("//tr[2]/td[2]"));
 
 
     }
